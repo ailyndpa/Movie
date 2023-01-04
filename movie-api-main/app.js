f82211b039982a06679f3f8d070fedc0d1cb4f14
ailyndpa <88048250+ailyndpa@users.noreply.github.com>
2023-01-04 13:37:47 -0300

    Delete movie-api-main directory

--- movie-api-main/app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-import api from './routes';
-
-app.use(express.json());
-app.use('/api', api);
-
-export default app;
--- movie-api-main/server.js
+++
@@ -1,2 +0,0 @@
-import app from './app';
-app.listen(3002, () => console.log('Server listening on port 3002'));
